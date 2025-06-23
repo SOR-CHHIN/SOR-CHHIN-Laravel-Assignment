15a4aab19f79dd739c3b2fd6883447158ec39139
SOR-CHHIN <sor.chhin@student.passerellesnumeriques.org>
2025-06-23 14:19:49 +0700

    Using validate:[CreateBookRequest, UpdateBookRequest] with database

--- Homework_01/app/Http/Controllers/BookController.php
+++ Homework_01/app/Http/Controllers/BookController.php
@@ -2,69 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateBookRequest;
+use App\Http\Requests\UpdateBookRequest;
 use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public $books = [
-        [
-            'id' => '1',
-            'title' => "The Great Gatsby",
-            'authorId' => 101,
-            'isbn' => "9780743273565",
-            'publicationYear' => 1925,
-            'genre' => "Classic",
-            'availableCopies' => 5
-        ],
-        [
-            'id' => '2',
-            'title' => "To Kill a Mockingbird",
-            'authorId' => 102,
-            'isbn' => "9780060935467",
-            'publicationYear' => 1960,
-            'genre' => "Fiction",
-            'availableCopies' => 3
-        ],
-        [
-            'id' => '3',
-            'title' => "1984",
-            'authorId' => 103,
-            'isbn' => "9780451524935",
-            'publicationYear' => 1949,
-            'genre' => "Dystopian",
-            'availableCopies' => 4
-        ],
-        [
-            'id' => '4',
-            'title' => "Pride and Prejudice",
-            'authorId' => 104,
-            'isbn' => "9780141439518",
-            'publicationYear' => 1813,
-            'genre' => "Romance",
-            'availableCopies' => 2
-        ],
-        [
-            'id' => '5',
-            'title' => "The Hobbit",
-            'authorId' => 105,
-            'isbn' => "9780345339683",
-            'publicationYear' => 1937,
-            'genre' => "Fantasy",
-            'availableCopies' => 6
-        ]
-    ];
-
-
+   
     public function index()
     
     {
         $book = new Book();
         return response()->json([
-            'message' => "get all books",
+            'message' => 'get all books',
             'data' => Book::all(),
         ], 200);
     }
@@ -73,25 +24,15 @@ class BookController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create(Request $request)
-    {
-        $book = Book::create([
-            'title'=>$request->title,
-            'author'=>$request-> author,
-            'published_year'=>$request->published_year
+    public function create(CreateBookRequest $request){
+        $book = Book::create($request->all());
 
-        ]);
-        if($book){
-             return response()->json([
-            'message' => 'create successfuly',
-            'data' => $book
-                
-        ], 201); 
-        }
         return response()->json([
-            'message'=>'Book create failed'
-        ],203);
-      
+            'message'=>'Book create successfully',
+            'data'=> $book
+        ],201);
+
+
     }
 
 
@@ -100,16 +41,14 @@ class BookController extends Controller
         $book = Book::where('id',$id)->get();
         if($book){
             return response()-> json([
-                'message'=>"Book show success",
+                'message'=>'Book show success',
                 'data'=>$book
             ],200);
         }
-        return response()->json([
-            'message'=> "book cannot show"
-        ],203);
+       
     }
 
-    public function update(Request $request, $id)
+    public function update(UpdateBookRequest $request, $id)
     {
        $book = Book::where('id',$id)->update([
         'title'=>$request->title,
@@ -118,7 +57,7 @@ class BookController extends Controller
        ]);
        if($book){
            return response()->json([
-                'message'=> "book updated successfully",
+                'message'=> 'book updated successfully',
                 'data'=>$book
             ],201);
        }
@@ -132,12 +71,10 @@ class BookController extends Controller
      */
     public function delete(string $id)
     {
-        $book = Book::where('id',$id)->delete([
-
-        ]);
+        $book = Book::where('id',$id)->delete();
         if($book){
             return response()->json([
-                'message'=>"delete book success",
+                'message'=>'delete book success',
                 'data' => $book
             ],200);
         }
---
+++ Homework_01/app/Http/Requests/UpdateBookRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+    protected function failedValidation(Validator $validator)
+    {
+       throw new HttpResponseException(
+        response()->json([
+            'successfully'=> false,
+            'message'=> $validator->errors()
+
+        ],512)
+       ) ;
+    }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title'=>'required|string|min:2|max:255',
+           'author'=> 'string',
+           'published_year'=>'integer'
+        ];
+    }
+}
